a7d5d698fd0b72b53e7211fdf1b61c46d3e4f5ef
saragee3 <sgee03@gmail.com>
2021-05-12 14:12:04 -0700

    tests

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,16 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
+fn plus_two(num: i32) -> i32 {
+    num + 2
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(5, plus_two(3));
     }
 }
